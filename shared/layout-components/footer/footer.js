14d1cd8211caa2ccee356bfa46c569c9f593affc
Syed <salizahid75@gmail.com>
2023-05-29 17:00:07 +0500

    Feat: Remove Extras from Dashboard

--- shared/layout-components/footer/footer.js
+++ shared/layout-components/footer/footer.js
@@ -1,24 +1,24 @@
-import React, { Fragment } from 'react';
-import { Col, Row } from 'react-bootstrap';
+import React, {Fragment} from 'react';
+import {Col, Row} from 'react-bootstrap';
 import Link from 'next/link';
 
 const Footer = () => {
-
   return (
-
-  <Fragment >
-    <footer className="footer mt-3  ">
-      <div className="container">
-        <Row className="align-items-center flex-row-reverse">
-          <Col md={12} sm={12} className="text-center">
-            Copyright © 2023 <Link href="#!">Sash</Link>. Designed with <span className="fa fa-heart text-danger"></span> by <Link href="#!"> Spruko </Link> All rights reserved.
-          </Col>
-        </Row>
-      </div>
-    </footer>
-    {/* <!-- FOOTER CLOSED --> */}
-  </Fragment>
-);
-  };
+    <Fragment>
+      <footer className="footer mt-3  ">
+        <div className="container">
+          <Row className="align-items-center flex-row-reverse">
+            <Col md={12} sm={12} className="text-center">
+              Copyright © 2023 <Link href="#!">Tip Your Teacher</Link>. Designed
+              with <span className="fa fa-heart text-danger"></span> by{' '}
+              <Link href="#!"> Desol Int. </Link> All rights reserved.
+            </Col>
+          </Row>
+        </div>
+      </footer>
+      {/* <!-- FOOTER CLOSED --> */}
+    </Fragment>
+  );
+};
 
 export default Footer;
