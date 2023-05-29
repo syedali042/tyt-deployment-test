ea5d535b455b338048a4802f32b327439708312b
Syed <salizahid75@gmail.com>
2023-05-29 14:45:21 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: true,
+  basePath: "",
+  assetPrefix: "",
+  images: {
+    loader: 'akamai',
+    path: ''
+  }
+};
+
+module.exports = nextConfig;
---
+++ shared/layout-components/sidebar/sidemenu.js
@@ -0,0 +1,15 @@
+export const MENUITEMS = [
+  {
+    menutitle: 'MAIN',
+    Items: [
+      {
+        path: `/components/dashboard/dashboard`,
+        icon: 'fe fe-home',
+        type: 'link',
+        active: false,
+        selected: false,
+        title: 'Dashboard',
+      },
+    ],
+  },
+];
