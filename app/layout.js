9cf364278add48fadf0ef5514238b812af300d94
Syed <salizahid75@gmail.com>
2023-06-02 16:41:27 +0500

    Feat: Redux Added To Layout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import '@/styles/globals.scss';
-
+import {Providers} from '@/shared/redux/provider';
 export default function RootLayout({children}) {
   return (
     <html lang="en">
       <title>Tip your teacher</title>
       <link rel="icon" href={'../../../assets/images/brand/favicon.ico'} />
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
