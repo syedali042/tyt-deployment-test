f48158f19c4dba5b08c1074b3a8698291d5a5c5f
Syed <salizahid75@gmail.com>
2023-07-24 14:19:14 +0500

    Fix: location not defined next js error

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,11 @@
 'use client';
 import {useRouter} from 'next/navigation';
+import {useEffect} from 'react';
 
 const Dashboard = () => {
   const router = useRouter();
-  router.push('/dashboard/home');
+  useEffect(() => {
+    router.push('/dashboard/home');
+  }, []);
 };
-
 export default Dashboard;
