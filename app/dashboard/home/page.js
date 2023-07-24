fae4ad779ee5f5f0c5b85e4e5ece2150156c8302
Syed <salizahid75@gmail.com>
2023-07-24 17:54:16 +0500

    Feat: Summary widgets added in home page

--- app/dashboard/home/page.js
+++ app/dashboard/home/page.js
@@ -3,11 +3,13 @@ import React from 'react';
 // For Later Use
 import PageHeader from '@/shared/layout-components/pageheader/pageHeader';
 import Contentlayout from '@/shared/layout-components/layout/content-layout';
+import DashboardSummary from '@/shared/components/dashboard/summary/DashboardSummary';
 
 const DashboardHome = () => {
   return (
     <Contentlayout>
       <PageHeader titles="" active="Home" items={['Dashboard']} />
+      <DashboardSummary />
     </Contentlayout>
   );
 };
