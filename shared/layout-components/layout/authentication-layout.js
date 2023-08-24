9da5687aff77830cd5be03ff5e49c527a4a6de6f
Syed <salizahid75@gmail.com>
2023-08-24 17:49:03 +0500

    Fix: Auth blink when user logged in

--- shared/layout-components/layout/authentication-layout.js
+++ shared/layout-components/layout/authentication-layout.js
@@ -2,10 +2,22 @@
 import SSRProvider from 'react-bootstrap/SSRProvider';
 import {useSelector} from 'react-redux';
 import {getCurrentUser} from '@/shared/redux/slices/user';
+import {useState} from 'react';
+import {useEffect} from 'react';
 
 const Authenticationlayout = ({children}) => {
   const currentUser = useSelector(getCurrentUser);
-  return !currentUser?.userInternalId && <SSRProvider>{children}</SSRProvider>;
+  const [display, setDisplay] = useState('none');
+  useEffect(() => {
+    if (currentUser == null) setDisplay('block');
+  }, [currentUser]);
+  return (
+    !currentUser && (
+      <SSRProvider>
+        <div style={{display}}>{children}</div>
+      </SSRProvider>
+    )
+  );
 };
 
 export default Authenticationlayout;
