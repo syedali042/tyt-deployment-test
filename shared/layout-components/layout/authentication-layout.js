81fd582b60701f3e7c88d71db29a26f2709415ed
syedali042 <salizahid75@gmail.com>
2023-08-29 01:45:30 +0500

    Fix: remove extra condition

--- shared/layout-components/layout/authentication-layout.js
+++ shared/layout-components/layout/authentication-layout.js
@@ -1,21 +1,13 @@
 'use client';
 import SSRProvider from 'react-bootstrap/SSRProvider';
 import {useSelector} from 'react-redux';
-import {
-  getCurrentUser,
-  getIsCurrentUserInitialValue,
-} from '@/shared/redux/slices/user';
+import {getCurrentUser} from '@/shared/redux/slices/user';
 
 const Authenticationlayout = ({children}) => {
   const currentUser = useSelector(getCurrentUser);
-  const isCurrentUserInitialValue = useSelector(getIsCurrentUserInitialValue);
 
   return (
-    <SSRProvider>
-      {currentUser === null && !isCurrentUserInitialValue && (
-        <div>{children}</div>
-      )}
-    </SSRProvider>
+    <SSRProvider>{currentUser === null && <div>{children}</div>}</SSRProvider>
   );
 };
 
